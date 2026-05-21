1c7df78dafd4d8f67d390fb63a045ef830d18ef0
bianca255 <b.haguma@alustudent.com>
2026-05-21 08:00:55 +0300

    Fix logic errors and pycodestyle warnings

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -2,6 +2,8 @@
 """Module to create the forward propagation graph."""
 import tensorflow as tf
 
+create_layer = __import__('1-create_layer').create_layer
+
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
@@ -15,10 +17,7 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     Returns:
         the prediction of the network in tensor form
     """
-    create_layer = __import__('1-create_layer').create_layer
-
     prev = x
     for i in range(len(layer_sizes)):
         prev = create_layer(prev, layer_sizes[i], activations[i])
     return prev
-
